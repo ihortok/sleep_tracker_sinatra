d92e4234546e16886fda83a2496c1bcc67624190
ihortok <itokaryk@gmail.com>
2021-09-05 00:39:55 +0300

    finish sleep

--- app/controllers/sleeps_controller.rb
+++ app/controllers/sleeps_controller.rb
@@ -5,11 +5,17 @@ class SleepsController < ApplicationController
   get '/sleeps/new' do
     redirect '/users/sign_in' unless logged_in?
 
+    set_running_sleep
+
     erb :'sleeps/new.html', layout: :'layout.html'
   end
 
   post '/sleeps/create' do
     redirect '/users/sign_in' and return unless logged_in?
+
+    set_running_sleep
+
+    redirect '/sleeps/new' if @running_sleep
 
     sleep = Sleep.new(sleep_params)
 
@@ -20,11 +26,33 @@ class SleepsController < ApplicationController
     end
   end
 
+  post '/sleeps/finish' do
+    redirect '/users/sign_in' and return unless logged_in?
+
+    set_running_sleep
+
+    redirect '/sleeps/new' unless @running_sleep
+
+    sleep_finished_at = Time.new(
+      params['sleep_finished_at']['year'],
+      params['sleep_finished_at']['month'],
+      params['sleep_finished_at']['day'],
+      params['sleep_finished_at']['hour'],
+      params['sleep_finished_at']['minute'],
+      0,
+      time_zone_offset
+    )
+
+    if @running_sleep.update({ finished_at: sleep_finished_at, status: :finished })
+      redirect '/dashboard'
+    else
+      erb :'dashboard.html', layout: :'layout.html', locals: { message: 'Something went wrong. Please try again.' }
+    end
+  end
+
   private
 
   def sleep_params
-    time_zone_offset = TZInfo::Timezone.get(current_user.time_zone).utc_offset
-
     sleep_started_at = Time.new(
       params['sleep_started_at']['year'],
       params['sleep_started_at']['month'],
@@ -59,5 +87,13 @@ class SleepsController < ApplicationController
       finished_at: sleep_finished_at,
       status: status
     }
+  end
+
+  def time_zone_offset
+    @time_zone_offset ||= TZInfo::Timezone.get(current_user.time_zone).utc_offset
+  end
+
+  def set_running_sleep
+    @running_sleep = current_user.baby.sleeps.running.first
   end
 end
