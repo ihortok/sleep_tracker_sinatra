88217b72ad254062e746672fc4091f523b4d4dd2
ihortok <itokaryk@gmail.com>
2021-09-25 23:52:09 +0300

    baby controller

---
+++ app/controllers/baby_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# BabyController
+class BabyController < ApplicationController
+  get '/baby' do
+    redirect '/users/sign_in' unless logged_in?
+
+    redirect '/baby/new' unless current_user.baby.present?
+
+    @baby = current_user.baby
+
+    erb :'baby/show.html', layout: :'layout.html'
+  end
+
+  get '/baby/new' do
+    redirect '/users/sign_in' unless logged_in?
+
+    redirect '/baby/show' if current_user.baby.present?
+
+    erb :'baby/new.html', layout: :'layout.html'
+  end
+
+  post '/baby/create' do
+    redirect '/users/sign_in' unless logged_in?
+
+    redirect '/dashboard' if current_user.baby.present?
+
+    baby = Baby.new(params.merge(user: current_user))
+
+    if baby.save
+      redirect '/dashboard'
+    else
+      erb :'baby/new.html', locals: { message: 'Something went wrong. Please try again.' }
+    end
+  end
+end
